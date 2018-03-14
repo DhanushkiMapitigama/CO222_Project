f1a4338ab55fe35d6706d0d3c79eb49e130afa56
DhanushkiMapitigama <dhanumapitigama@gmail.com>
2018-03-14 09:26:31 +0530

    node struct

---
+++ project2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+typedef struct node {
+	char word[100];
+	int count;
+	struct node* next;
+
+} list;
+
+int main(int argc, char **argv ){
+	return 0;
+}
